5470fd26280a0da08aa08582c8b68ce3cec4b36a
Takaaki Kanda <asuasu.tee@gmail.com>
2023-08-23 12:09:43 +0900

    test2をroleによる表示非表示Componentに紐付け

--- force-app/main/default/lwc/test2/test2.js
+++ force-app/main/default/lwc/test2/test2.js
@@ -1,16 +1,20 @@
-import { LightningElement, api, wire, track } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import { getRecord,updateRecord } from 'lightning/uiRecordApi';
+import { LightningElement, wire, track, api } from 'lwc';
+import userId from '@salesforce/user/Id';
+import { getRecord } from 'lightning/uiRecordApi';
+import test2Page from './test2.html';
+import nullPage from './null.html';
 
 export default class Test2 extends LightningElement {
-  @api recordId
+  userId = userId
+  @api role
+
   @track record
   @track error
 
-  // 現在の値を取得
-  @wire(getRecord, {recordId: '$recordId',fields: ["Account.SLAExpirationDate__c"]})
+  @wire(getRecord, { 'recordId': userId, fields: "User.UserRole.Name" })
   handleRecord({ data, error }) {
     if (data) {
+      console.log(data)
       this.record = data;
       this.error = undefined;
     } else if (error) {
@@ -19,66 +23,13 @@ export default class Test2 extends LightningElement {
     }
   }
 
-  
-  connectedCallback() {
-    
-  }
-
-  
-  renderedCallback() {
-    
-  }
-
-  
-  get value() {
-    return this.record.fields.SLAExpirationDate__c.value
-  }
-
-  
-  async handleUpdate(e) {
-    // Validation
-    const allValid = [...this.template.querySelectorAll('edit-target')]
-        // 第１:これまでの結果, 第２:配列の値
-        .reduce((validSoFar, inputFields) => {
-          inputFields.reportValidity();
-          return validSoFar && inputFields.checkValidity();
-        }, true); // 初期値
-    
-    if (allValid) {
-      // 更新するためのデータを作成
-      const fields = {}
-      fields['Id'] = this.recordId;
-      fields["SLAExpirationDate__c"] = e.target.value;
-      const recordInput = {fields};
-
-      try {
-        await updateRecord(recordInput)
-        this.dispatchEvent(
-          new ShowToastEvent({
-              title: 'Success',
-              message: 'Contact updated',
-              variant: 'success'
-          })
-        );
-      } catch(error) {
-        this.dispatchEvent(
-          new ShowToastEvent({
-              title: 'Error updating record',
-              message: error.body.message,
-              variant: 'error'
-          })
-        );
-      }
-    } else {
-      this.dispatchEvent(
-        new ShowToastEvent({
-            title: 'Something is wrong',
-            message: 'Check your input and try again.',
-            variant: 'error'
-        })
-      );
+  render() {
+    if (!this.record || !this.record.fields) {
+      return nullPage;
     }
+    if (this.record.fields.UserRole.displayValue === this.role || this.role === 'free') {
+      return test2Page;
+    }
+    return nullPage;
   }
-  
-  
 }
